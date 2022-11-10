7dc4433df1955b6be2d468d84bfeb474025fbb72
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-11-10 01:21:32 +0000

    Getline
    function getline gets input line when
    the program is run

--- shell.c
+++ shell.c
@@ -6,5 +6,18 @@
 */
 int main(void)
 {
+	char *lineptr;
+	size_t n;
+	size_t n_read;
+
+	lineptr = NULL;
+	n = 0;
+
+	do {
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "($) ", 4);
+	} while ((n_read = getline(&lineptr, &n, stdin)) != -1 && (n_read != EOF));
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
 	return (0);
 }
